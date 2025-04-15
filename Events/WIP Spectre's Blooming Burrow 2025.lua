dc57a3005c5397e8a37f9828567b74b7dd9a32f9
spectre011-code <spectre011@protonmail.com>
2025-04-15 03:23:12 -0300

    Update WIP Spectre's Blooming Burrow 2025.lua

--- Events/WIP Spectre's Blooming Burrow 2025.lua
+++ Events/WIP Spectre's Blooming Burrow 2025.lua
@@ -115,7 +115,7 @@ local function CreateGUI()
     AddBackground("Background", 0.90, 1, ImColor.new(15, 13, 18, 255))
     AddLabel("Author/Version", ScriptName .. " v" .. ScriptVersion .. " by " .. Author, ImColor.new(238, 230, 0))
     AddLabel("ActivityComboBoxLabel", "Select an activity:", ImColor.new(255, 255, 255))
-    local options = {"- none - ", "Beekeeping", "Carrot Boating", "Chocolate Mining", "Smith Shiny Foil", "Hunt Chocolate Bunnies", "Egg Plant Production Line"}
+    local options = {"- none - ", "Beekeeping", "Carrot Boating", "Chocolate Mining", "Smith Shiny Foil", "Cocoamancy", "Egg Plant Production Line"}
     AddComboBox("ActivityComboBox", " ", options)
     AddLabel("Status", "Status: " .. CurrentStatus, ImColor.new(238, 230, 0))
 end
@@ -127,7 +127,7 @@ local function SetComboBoxOption()
     if SelectedOption == "Carrot Boating" then stageID = 2 end
     if SelectedOption == "Chocolate Mining" then stageID = 3 end
     if SelectedOption == "Smith Shiny Foil" then stageID = 4 end
-    if SelectedOption == "Hunt Chocolate Bunnies" then stageID = 5 end
+    if SelectedOption == "Cocoamancy" then stageID = 5 end
     if SelectedOption == "Egg Plant Production Line" then stageID = 6 end
 end
 
@@ -162,18 +162,41 @@ local IDS = {
         ["FlowerFoodSack"] = {132747},
         ["EnragedWasp"] = {31785},
         ["WaspTraps"] = {132746},
-        ["PileOfSmokers"] = {132748},
-        
+        ["PileOfSmokers"] = {132748}        
+    },
+    ["CarrotBoating"] = {
+        ["Coordinates"] = {
+            {x = 3805, y = 4937, z = 0}, --Starting
+            {x = 3788, y = 4935, z = 0}, --After pole
+            {x = 3812, y = 4923, z = 0}, --After hoop
+            {x = 3812, y = 4925, z = 0} -- After ramp
+        }
+    },
+    ["SmithShinyFoil"] = {
+        ["SoftMetalIngot"] = {56551},
+        ["SoftenedMetalIngot"] = {56552},
+        ["Foil"] = {56553}
+    },
+    ["Cocoamancy"] = {
+        ["ChocolateBurrowFilled"] = {129765}
     }
+
 }
---Simple sleep
----@param seconds number
----@return boolean
+
 local function Sleep(seconds)
     local endTime = os.clock() + seconds
     while os.clock() < endTime do
     end
     return true
+end
+
+local function IsPlayerAtCoords(x, y, z)
+    local coord = API.PlayerCoord()
+    if x == coord.x and y == coord.y and z == coord.z then
+        return true
+    else
+        return false
+    end
 end
 
 local function IsPlayerInArea(x, y, z, radius)
@@ -198,12 +221,8 @@ local function MoveTo(X, Y, Z, Tolerance)
     return true
 end
 
-local function QtittyWiltedDaffodil()
-    return #API.GetAllObjArray1(IDS["Beekeeping"]["WiltedDaffodil"], 50, {0})
-end
-
-local function QtittyWasps()
-    return #API.GetAllObjArray1(IDS["Beekeeping"]["EnragedWasp"], 50, {1})
+local function DialogBoxOpen()
+    return API.VB_FindPSett(2874, 1, 0).state == 12
 end
 
 local StageDescriptions = {
@@ -211,7 +230,7 @@ local StageDescriptions = {
     [2] = "Carrot Boating",
     [3] = "Chocolate Mining",
     [4] = "Smith Shiny Foil",
-    [5] = "Hunt Chocolate Bunnies",
+    [5] = "Cocoamancy",
     [6] = "Egg Plant Production Line"
 }
 
@@ -234,6 +253,14 @@ local stageFunctions = {
             return false
         end
 
+        local function QtittyWiltedDaffodil()
+            return #API.GetAllObjArray1(IDS["Beekeeping"]["WiltedDaffodil"], 50, {0})
+        end
+        
+        local function QtittyWasps()
+            return #API.GetAllObjArray1(IDS["Beekeeping"]["EnragedWasp"], 50, {1})
+        end
+
         if not API.PInArea21(3799, 3821, 4888, 4904) then
             UpdateStatus("Moving to Beekeeping area")
             print("Moving to Beekeeping area")
@@ -242,6 +269,7 @@ local stageFunctions = {
             print("Already in Beekeeping area")
             --Makes sure the player always have a smoker in inventory
             while API.Read_LoopyLoop() and Inventory:GetItemAmount(58736) < 1 do
+                UpdateStatus("Picking up smoker")
                 print("Picking up smoker")
                 API.DoAction_Object1(0x2d,API.OFF_ACT_GeneralObject_route0,IDS.Beekeeping.PileOfSmokers,50)
                 API.RandomSleep2(3000, 300, 300)
@@ -251,6 +279,7 @@ local stageFunctions = {
             --Handles wasps
             if QtittyWasps() > 0 then
                 print("Handling wasps")
+                UpdateStatus("Handling wasps")
                 while API.Read_LoopyLoop() and Inventory:GetItemAmount(58734) < 5 do
                     API.DoAction_Object1(0x2d,API.OFF_ACT_GeneralObject_route0,IDS.Beekeeping.WaspTraps,50)
                     API.RandomSleep2(3000, 300, 300)
@@ -264,6 +293,7 @@ local stageFunctions = {
             --Handles feeding flowers
             if QtittyWiltedDaffodil() > 0 then
                 print("Wilted Daffodils found. Feeding flowers")
+                UpdateStatus("Feeding flowers")
                 while API.Read_LoopyLoop() and Inventory:GetItemAmount(58735) < 5 do
                     print("Getting flower food")
                     API.DoAction_Object1(0x2d,API.OFF_ACT_GeneralObject_route0,IDS.Beekeeping.FlowerFoodSack,50)
@@ -281,6 +311,7 @@ local stageFunctions = {
             --Handles tending to bees
             if not IsTendingToBees() then
                 print("Interacting with Beehive")
+                UpdateStatus("Beekeeping")
                 Interact:Object("Beehive", "Tend to", 50)
                 API.RandomSleep2(3000, 300, 300)
                 API.WaitUntilMovingEnds(20, 2)
@@ -291,21 +322,151 @@ local stageFunctions = {
     [2] = function()
         print("Stage 2: Carrot Boating")
         UpdateStatus("Carrot Boating")
+
+        local function WhereAmI()
+            if IsPlayerInArea(3802, 4945, 0, 2) then
+                return "Not in boat"
+            end
+        
+            if IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[1].x, IDS["CarrotBoating"].Coordinates[1].y, IDS["CarrotBoating"].Coordinates[1].z) then
+                return "Starting"
+            end
+        
+            if IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[2].x, IDS["CarrotBoating"].Coordinates[2].y, IDS["CarrotBoating"].Coordinates[2].z) then
+                return "After pole"
+            end
+        
+            if IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[3].x, IDS["CarrotBoating"].Coordinates[3].y, IDS["CarrotBoating"].Coordinates[3].z) then
+                return "After hoop"
+            end
+        
+            if IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[4].x, IDS["CarrotBoating"].Coordinates[4].y, IDS["CarrotBoating"].Coordinates[4].z) then
+                return "After ramp"
+            end
+
+            return "idk"
+        end
+
+        if WhereAmI() == "idk" then
+            UpdateStatus("Moving to Carrot Boating area")
+            print("Moving to Carrot Boating area")
+            MoveTo(3802, 4945, 0, 1)
+        elseif WhereAmI() == "Not in boat" then
+            UpdateStatus("Renting boat")
+            print("Renting boat")
+            Interact:Object("Boat rental", "Rent boat", 10)
+            UTILS.randomSleep(1000)
+            if DialogBoxOpen() then
+                API.KeyboardPress2(0x31, 40, 60)
+                UTILS.randomSleep(5000)
+            end
+        elseif WhereAmI() == "Starting" then
+            UpdateStatus("Crossing slalom poles")
+            print("Crossing slalom poles")
+            Interact:Object("Slalom poles", "Navigate", 10)
+            while API.Read_LoopyLoop() and not IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[2].x, IDS["CarrotBoating"].Coordinates[2].y, IDS["CarrotBoating"].Coordinates[2].z) do
+                UTILS.randomSleep(100)
+            end
+        elseif WhereAmI() == "After pole" then
+            UpdateStatus("Jumping hoop")
+            print("Jumping hoop")
+            Interact:Object("Hoop", "Jump through", 30)
+            while API.Read_LoopyLoop() and not IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[3].x, IDS["CarrotBoating"].Coordinates[3].y, IDS["CarrotBoating"].Coordinates[3].z) do
+                UTILS.randomSleep(100)
+            end
+        elseif WhereAmI() == "After hoop" then
+            UpdateStatus("Drifting ramp")
+            print("Drifting ramp")
+            Interact:Object("Ramp", "Drift in", 30)
+            while API.Read_LoopyLoop() and not IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[4].x, IDS["CarrotBoating"].Coordinates[4].y, IDS["CarrotBoating"].Coordinates[4].z) do
+                UTILS.randomSleep(100)
+            end
+        elseif WhereAmI() == "After ramp" then
+            UpdateStatus("Crossing slalom poles")
+            print("Crossing slalom poles")
+            Interact:Object("Slalom poles", "Navigate", 40)
+            while API.Read_LoopyLoop() and not IsPlayerAtCoords(IDS["CarrotBoating"].Coordinates[2].x, IDS["CarrotBoating"].Coordinates[2].y, IDS["CarrotBoating"].Coordinates[2].z) do
+                UTILS.randomSleep(100)
+            end
+        else
+            print("Something went wrong#########################")
+            API.Write_LoopyLoop(false)
+        end
     end,
 
     [3] = function()
         print("Stage 3: Chocolate Mining")
         UpdateStatus("Chocolate Mining")
+
+        if not API.PInArea21(3755, 3766, 4914, 4923) then
+            UpdateStatus("Moving to Chocolate Mining area")
+            print("Moving to Chocolate Mining area")
+            MoveTo(3762, 4917, 0, 1)
+        else
+            print("Already in Chocolate Mining area")
+            if Inventory:IsFull() then
+                print("Inventory is full. Depositing...")
+                UpdateStatus("Depositing chocolate")
+                Interact:Object("Chocolate mine shaft", "Deposit", 20)
+                while API.Read_LoopyLoop() and Inventory:IsFull() do
+                    UTILS.randomSleep(100)
+                end
+            else
+                print("Mining chocolate...")
+                UpdateStatus("Mining chocolate")
+                if not API.IsPlayerAnimating_(API.GetLocalPlayerName(), 3) then
+                    Interact:Object("Chocolate rock", "Mine", 20)
+                    API.RandomSleep2(3000, 300, 300)
+                    API.WaitUntilMovingEnds(20, 2)
+                end
+            end
+        end
     end,
 
     [4] = function()
         print("Stage 4: Smith Shiny Foil")
         UpdateStatus("Smith Shiny Foil")
+
+        if not API.PInArea21(3738, 3742, 4940, 4945) then
+            UpdateStatus("Moving to Smith Shiny Foil area")
+            print("Moving to Smith Shiny Foil area")
+            MoveTo(3740, 4943, 0, 1)
+        else
+            print("Already in Smith Shiny Foil area")
+            if not Inventory:IsFull() then
+                print("Taking soft metal ingots")
+                UpdateStatus("Taking soft metal ingots")
+                Interact:Object("Soft metal forge", "Take all from", 20)
+                while API.Read_LoopyLoop() and not Inventory:IsFull() do
+                    UTILS.randomSleep(100)
+                end
+            end
+            if Inventory:Contains(IDS.SmithShinyFoil.SoftMetalIngot) or Inventory:Contains(IDS.SmithShinyFoil.SoftenedMetalIngot) then
+                print("Smithing shiny foil...")
+                UpdateStatus("Smithing shiny foil")
+                if not API.IsPlayerAnimating_(API.GetLocalPlayerName(), 30) then
+                    print("Interacting with anvil")
+                    Interact:Object("Soft metal anvil", "Flatten", 20)
+                    API.RandomSleep2(3000, 300, 300)
+                end
+            elseif Inventory:Contains(IDS.SmithShinyFoil.Foil) then
+                print("Depositing foil")
+                UpdateStatus("Depositing foil")
+                Interact:Object("Foil storage", "Deposit", 20)
+                while API.Read_LoopyLoop() and Inventory:Contains(IDS.SmithShinyFoil.Foil) do
+                    UTILS.randomSleep(100)
+                end
+            else
+                print("Something went wrong@@@@@@@@@@@@@@@@@@@@@")
+                API.Write_LoopyLoop(false)
+            end
+        end
+
     end,
 
     [5] = function()
-        print("Stage 5: Hunt Chocolate Bunnies")
-        UpdateStatus("Hunt Chocolate Bunnies")
+        print("Stage 5: Cocoamancy")
+        UpdateStatus("Cocoamancy")
     end,
 
     [6] = function()
